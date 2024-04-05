040a9c8a48c59a92159ef9fb520986dc3248cb37
ProfessorX1900 <142480267+ProfessorX1900@users.noreply.github.com>
2024-04-05 11:19:22 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pydeck as pdk
 import pandas as pd
-from streamlit-navigation-bar import st_navbar
+from streamlit_navigation_bar import st_navbar
 
 st.set_page_config(page_title='Pothole Detection', page_icon="🚧", layout="wide")
 
